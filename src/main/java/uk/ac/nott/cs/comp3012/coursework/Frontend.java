b6c3ba76d958521fa5f194e3c49a74ed23bd27a9
ferdswa <maximcarr03@hotmail.com>
2025-11-26 11:33:11 +0000

    -m: Split the array into subarrays

--- src/main/java/uk/ac/nott/cs/comp3012/coursework/Frontend.java
+++ src/main/java/uk/ac/nott/cs/comp3012/coursework/Frontend.java
@@ -1,23 +1,24 @@
 package uk.ac.nott.cs.comp3012.coursework;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenStream;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import uk.ac.nott.cs.comp3012.coursework.ast.Ast;
 import uk.ac.nott.cs.comp3012.coursework.ast.AstBuilder;
-import uk.ac.nott.cs.comp3012.coursework.ast.ParseTreePrinter;
 import uk.ac.nott.cs.comp3012.coursework.util.HashMapTable;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Frontend {
     public Ast runFrontend(String input){
         HashMapTable<Object,Object,Object> symbolTable = new HashMapTable<>();
         AstBuilder astBuilder = new AstBuilder();
         ArrayList<String> ast = astBuilder.buildAst(input);
+        ArrayList<String[]> pathAsNodes = new ArrayList<>();
+        for (String s : ast) {
+            pathAsNodes.add(s.split(":"));
+        }
+        for (String[] pathAsNode : pathAsNodes) {
+            System.out.println(Arrays.toString(pathAsNode));
+        }
 
         return null;
     }
