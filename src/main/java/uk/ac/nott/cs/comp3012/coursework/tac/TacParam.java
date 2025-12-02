60753530b842c3706493aaa399ec3910ba9bd4a7
ferdswa <maximcarr03@hotmail.com>
2025-12-02 12:33:20 +0000

    -d: Remove unusedfiles

--- src/main/java/uk/ac/nott/cs/comp3012/coursework/tac/TacParam.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.nott.cs.comp3012.coursework.tac;
-
-public sealed interface TacParam {
-    record Value(int value) implements TacParam {}
-    record Variable(String value) implements TacParam {}
-    record Constant(int value) implements TacParam {}
-    record StrConst(String value) implements TacParam {}
-}
